6e91b1c7352aa9837ffa2a6b6305aae2128fc11e
raoul-k <raoulhugo.kutil@plus.ac.at>
2025-07-16 11:01:21 +0200

    Add files via upload

---
+++ R/disorder_mpcs_comparison.R
@@ -0,0 +1,63 @@
+
+setwd("~/IDA Lab/Dissertation/Paper4-Cooperations/Codes_R/github")
+source("mpcs.R")
+
+# data
+csv_names <- c("delusional_disorder.csv",
+               "schizoaffective_disorder.csv",
+               "schizophrenia.csv",
+               "schizophreniform_disorder.csv",
+               "speech_sound_disorder.csv")
+disorder_list <- vector(mode="list", length=length(csv_names))
+
+# other data
+# "major_depressive_disorder_sample.csv",
+# "persistent_depressive_disorder_sample.csv",
+# "panic_disorder_sample.csv",
+# "generalized_anxiety_disorder_sample.csv",
+
+# reading the CSVs from the subfolder "disorders"
+for(i in 1:length(csv_names)){
+  disorder_list[[i]] <- read.csv(paste0("./disorders/", csv_names[i]), header=T)
+}
+
+# transforming and combining them
+df_all_disorder <- plyr::join_all(disorder_list,type="full",match="first")
+df_all_disorder[is.na(df_all_disorder)] <- 0
+df_all_disorder_names <- unique(df_all_disorder$name)
+sim_matrix <- diag(1, length((df_all_disorder_names)))
+
+# test
+# k <- 0, progress iterator
+for(i in 1:(length(df_all_disorder_names)-1)){
+  for(j in (i+1):length(df_all_disorder_names)){
+    selected_disorder1 <- df_all_disorder_names[i]
+    sd1 <- df_all_disorder[df_all_disorder$name==selected_disorder1,]
+    sd1 <- sd1[,c(T,colSums(sd1[!names(df_all_disorder)=="name"])>0)]
+    
+    selected_disorder2 <- df_all_disorder_names[j]
+    sd2 <- df_all_disorder[df_all_disorder$name==selected_disorder2,]
+    sd2 <- sd2[,c(T,colSums(sd2[!names(df_all_disorder)=="name"])>0)]
+    
+    merged <- merge(sd1, sd2, all=T)
+    merged[is.na(merged)] <- 0
+    
+    m_sd1 <- merged[merged$name==selected_disorder1,][,!names(merged)=="name"]
+    m_sd2 <- merged[merged$name==selected_disorder2,][,!names(merged)=="name"]
+    m_sd1 <- as.matrix(m_sd1)
+    m_sd2 <- as.matrix(m_sd2)
+    colnames(m_sd1) <- c()
+    colnames(m_sd2) <- c()
+    
+    sim_matrix[j,i] <- round(MPCS(m_sd1, m_sd2, agg="mean"), 3)
+    #k <- k + 1
+  }
+  # loop progress
+  # print(paste0( round(k/sum(1:(length(df_all_disorder_names)-1)),2)*100 ,"% done"))
+}
+
+colnames(sim_matrix) <- df_all_disorder_names
+rownames(sim_matrix) <- df_all_disorder_names
+
+sim_matrix[upper.tri(sim_matrix, diag=F)] <- NA
+show(sim_matrix)
---
+++ R/mpcs.R
@@ -0,0 +1,49 @@
+
+# cosine similarity helper functions
+# 1: fast cosine similarity calculation between vector and vector
+# 2: fast cosine similarity calculation between matrix and vector (with max by default)
+vectorCosSim <- function(v1, v2){
+  z <- v1 %*% v2
+  n <- sqrt(crossprod(v1)*crossprod(v2))
+  return( z/n )
+}
+matrixCosSim <- function(v, m, max=T){
+  z <- m %*% v
+  n <- sqrt(apply(m, 1, crossprod) %*% crossprod(v))
+  
+  if(max) return( max(z/n) )
+  else return( z/n )
+}
+
+# precursor to the maximum pairwise cosine similarity 
+# (one direction = not symmetric, m1 -> m2)
+preMPCS <- function(m1, m2, agg="mean"){
+  # convert matrices (or vectors) to a list
+  if(is.vector(m1)) m1 <- t(m1)
+  if(is.vector(m2)) m2 <- t(m2)
+  
+  # check if any row is only 0s, should not be possible
+  if( any(rowSums(m1)==0) | any(rowSums(m2)==0) ) return()
+  
+  # check if the permutations have the same length (symptoms)
+  if(ncol(m1) != ncol(m2)) return()
+  
+  # maximum cos sim from each row of m1 to each row of m2
+  sim <- sapply(1:nrow(m1), function(i) matrixCosSim(m1[i,], m2))
+  agg_sim <- switch(agg,
+             "mean"= mean(sim),
+             "med" = median(sim),
+             "max" = max(sim),
+             "min" = min(sim),
+             "topx"= mean( sort(sim[1:(floor(nrow*0.05))], decreasing=T) ),
+             "This aggregate doesnt exist for this function! (DPS)"
+  ) # wrong name input case at the end; "med", "min", "topx" unused/experimental
+  
+  return(agg_sim)
+}
+
+# maximum pairwise cosine similarity (symmetric)
+MPCS <- function(m1, m2, agg="mean"){
+  sym_sim <- max(preMPCS(m1, m2, agg), preMPCS(m2, m1, agg))
+  return(sym_sim)
+}
